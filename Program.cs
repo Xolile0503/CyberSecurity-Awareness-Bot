d8d9f4a461133241687031febe073a2fb349eaad
ST10488461 <ST10488461@rcconnect.edu.za>
2026-04-13 20:57:59 +0200

    Add files via upload
    added project files

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG_POE
+{
+    internal class Program
+    {
+
+        private static void Main()
+        {
+            AudioPlayer.PlayGreeting();
+            UIHelper.ShowLogo();
+
+            Console.Write("\nEnter your name: ");
+            string name = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(name))
+            {
+                Console.Write("Name cannot be empty. Enter your name: ");
+                name = Console.ReadLine();
+            }
+
+            Console.Clear();
+            UIHelper.ShowLogo();
+
+            Bot chatbot = new Bot(name);
+            chatbot.Start();
+        }
+    }
+}
+    
+
